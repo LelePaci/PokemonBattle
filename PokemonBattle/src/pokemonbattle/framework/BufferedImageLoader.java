c518beaf95c78c2331eca9281050af97a4a8d7c0
Emanuele <paciemanuele.2@gmail.com>
2021-12-17 15:11:43 +0100

    Spostata directory immagini e textures

--- PokemonBattle/src/pokemonbattle/framework/BufferedImageLoader.java
+++ PokemonBattle/src/pokemonbattle/framework/BufferedImageLoader.java
@@ -6,6 +6,7 @@
 package pokemonbattle.framework;
 
 import java.awt.image.BufferedImage;
+import java.io.FileInputStream;
 import java.io.IOException;
 import javax.imageio.ImageIO;
 
@@ -16,8 +17,8 @@ import javax.imageio.ImageIO;
 public class BufferedImageLoader {
     private BufferedImage image;
     
-    public BufferedImage laodImage(String path) throws IOException{
-        image = ImageIO.read(getClass().getResource(path));
+    public BufferedImage loadImage(String path) throws IOException{
+        image = ImageIO.read(new FileInputStream(path));
         return image;
     }
 }
