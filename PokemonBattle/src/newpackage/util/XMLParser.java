2dcfbb4714741cb9cf8a1a23865c70483dbd164e
Mattia Demaria <demaria_mattia@ismonnet.onmicrosoft.com>
2021-12-21 12:53:28 +0100

    Aggiunta classe parser file XML

---
+++ PokemonBattle/src/newpackage/util/XMLParser.java
@@ -0,0 +1,40 @@
+package newpackage.util;
+
+public class XMLParser {
+
+    String test = "<pokemon>"
+            + "    <nome>Charizard</nome>"
+            + "    <vita>300</vita>"
+            + "    <tipo>Fuoco, Volante</tipo>"
+            + "    <image>res/pokedex/images/charizard-front.png</image>"
+            + "    <mosse>"
+            + "        <mossa>"
+            + "            <nome>Graffio</nome>"
+            + "            <tipo>Normale</tipo>"
+            + "            <danni>15</danni>"
+            + "            <utilizziMax>35</utilizziMax>"
+            + "        </mossa>"
+            + "        <mossa>"
+            + "            <nome>Braciere</nome>"
+            + "            <tipo>Fuoco</tipo>"
+            + "            <danni>20</danni>"
+            + "            <utilizziMax>40</utilizziMax>"
+            + "        </mossa>"
+            + "        <mossa>"
+            + "            <nome>Lanciafiamme</nome>"
+            + "            <tipo>Fuoco</tipo>"
+            + "            <danni>30</danni>"
+            + "            <utilizziMax>15</utilizziMax>"
+            + "        </mossa>"
+            + "        <mossa>"
+            + "            <nome>Dragospiro</nome>"
+            + "            <tipo>Drago</tipo>"
+            + "            <danni>30</danni>"
+            + "            <utilizziMax>20</utilizziMax>"
+            + "        </mossa>"
+            + "    </mosse>"
+            + "    <status></status>"
+            + "</pokemon>";
+    
+    
+}
--- PokemonBattle/src/pokemonbattle/peer/TCPClient.java
+++ PokemonBattle/src/pokemonbattle/peer/TCPClient.java
@@ -1,4 +1,38 @@
 package pokemonbattle.peer;
-public class TCPClient {
-    
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+
+public class TCPClient extends Thread {
+
+    @Override
+    public void run() {
+        Socket socket = null;
+        try {
+            InetAddress addr = InetAddress.getByName("localhost");
+            socket = new Socket(addr, 4269);
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())), true);
+
+            out.println("CIAO");
+            String inputLine = in.readLine();
+            System.out.println(inputLine);
+            out.println("exit");
+            inputLine = in.readLine();
+            System.out.println(inputLine);
+            socket.close();
+        } catch (IOException ex) {
+        } finally {
+        }
+        try {
+            socket.close();
+        } catch (IOException ex) {
+        }
+    }
 }
